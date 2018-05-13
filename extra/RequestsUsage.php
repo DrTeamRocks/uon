4906a0e8cff1f89cad0ae14c71b1636f9edd7e69
Paul Rock <paul@drteam.rocks>
2018-05-13 15:01:22 +0300

    extra example with getDocument of requiest class added

--- extra/RequestsUsage.php
+++ extra/RequestsUsage.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $config = new \UON\Config();
 $config
-    ->set('token', 'YOUR_TOKEN')
+    ->set('token', file_get_contents(__DIR__ . '/../tests/_token.txt'))
     ->set('timeout', 10);
 
 $uon = new \UON\API($config);
@@ -19,6 +19,12 @@ $date = $uon->requests->getDate('2018-03-01', '2018-03-30');
 
 // Get all requests from dates range, for some source id
 $dateSource = $uon->requests->getDate('2017-06-01', '2017-06-10', '1');
+
+// Get the document, for this need to know IDs of template and request
+$doc = $uon->requests->getDocument([
+    'template_id' => '45',
+    'request_id' => '69',
+]);
 
 // Data array of new user (or details for update)
 $request = [

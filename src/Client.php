c7b54297b6ffc74d29d70f2aa22c1472476152ab
Paul Rock <paul@drteam.rocks>
2021-02-13 12:19:12 +0300

    Client class simplification, code style fine-tune

--- src/Client.php
+++ src/Client.php
@@ -2,8 +2,11 @@
 
 namespace UON;
 
+use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\RequestOptions;
 use Psr\Http\Message\ResponseInterface;
+use UON\Exceptions\UonEmptyResponseException;
+use UON\Exceptions\UonTooManyRequests;
 use UON\Interfaces\QueryInterface;
 
 /**
@@ -11,24 +14,36 @@ use UON\Interfaces\QueryInterface;
  * @link    http://drteam.rocks
  * @license MIT
  * @package UON
- * @property \UON\Endpoints\Bcard     $bcard      Bonus cards
- * @property \UON\Endpoints\Cash      $cash       Money operations
- * @property \UON\Endpoints\Catalog   $catalog    Catalog of products
- * @property \UON\Endpoints\Chat      $chat       For work with chat messages
- * @property \UON\Endpoints\Cities    $cities     Cities of countries
- * @property \UON\Endpoints\Countries $countries  Work with countries
- * @property \UON\Endpoints\Hotels    $hotels     Hotels methods
- * @property \UON\Endpoints\Leads     $leads      Details about clients
- * @property \UON\Endpoints\Misc      $misc       Optional single methods
- * @property \UON\Endpoints\Nutrition $nutrition  Some methods about eat
- * @property \UON\Endpoints\Payments  $payments   Payment methods
- * @property \UON\Endpoints\Reminders $reminders  Work with reminders
- * @property \UON\Endpoints\Requests  $requests   New requests from people
- * @property \UON\Endpoints\Sources   $sources    All available sources
- * @property \UON\Endpoints\Services  $services   All available sources
- * @property \UON\Endpoints\Statuses  $statuses   Request statuses
- * @property \UON\Endpoints\Suppliers $suppliers  External companies
- * @property \UON\Endpoints\Users     $users      For work with users
+ *
+ * @property \UON\Endpoints\Avia           $avia           Aero transfer
+ * @property \UON\Endpoints\Bcard          $bcard          Bonus cards
+ * @property \UON\Endpoints\CallHistory    $callHistory    History of calls
+ * @property \UON\Endpoints\Cash           $cash           Money operations
+ * @property \UON\Endpoints\Catalog        $catalog        Catalog of products
+ * @property \UON\Endpoints\Chat           $chat           For work with chat messages
+ * @property \UON\Endpoints\Cities         $cities         Cities of countries
+ * @property \UON\Endpoints\Countries      $countries      Work with countries
+ * @property \UON\Endpoints\Currencies     $currencies     Work with currencies
+ * @property \UON\Endpoints\ExtendedFields $extendedFields For manipulation with extended fields
+ * @property \UON\Endpoints\Hotels         $hotels         Hotels methods
+ * @property \UON\Endpoints\Leads          $leads          Details about clients
+ * @property \UON\Endpoints\Mails          $mails          For work with emails
+ * @property \UON\Endpoints\Managers       $managers       Get access to list of managers (sale operators)
+ * @property \UON\Endpoints\Misc           $misc           Optional single methods
+ * @property \UON\Endpoints\Notifications  $notifications  For creating new notifications for managers
+ * @property \UON\Endpoints\Nutrition      $nutrition      Some methods about eat
+ * @property \UON\Endpoints\Offices        $offices        Get access to list of all offices
+ * @property \UON\Endpoints\Payments       $payments       Payment methods
+ * @property \UON\Endpoints\ReasonsDeny    $reasonsDeny    List of all deny reasons
+ * @property \UON\Endpoints\Reminders      $reminders      Work with reminders
+ * @property \UON\Endpoints\Requests       $requests       New requests from people
+ * @property \UON\Endpoints\Services       $services       All available services
+ * @property \UON\Endpoints\Sources        $sources        All available sources
+ * @property \UON\Endpoints\Statuses       $statuses       Request statuses
+ * @property \UON\Endpoints\Suppliers      $suppliers      External companies
+ * @property \UON\Endpoints\Users          $users          For work with users
+ * @property \UON\Endpoints\Visa           $visa           For work with visa statuses
+ * @property \UON\Endpoints\Webhooks       $webhooks       Webhooks management
  */
 class Client implements QueryInterface
 {
@@ -105,7 +120,7 @@ class Client implements QueryInterface
      *
      * @return null|\GuzzleHttp\Client
      */
-    public function getClient(): ?\GuzzleHttp\Client
+    public function getClient(): ?HttpClient
     {
         return $this->client;
     }
@@ -117,9 +132,9 @@ class Client implements QueryInterface
      *
      * @return \GuzzleHttp\Client
      */
-    public function initClient(array $configs = []): \GuzzleHttp\Client
+    public function initClient(array $configs = []): HttpClient
     {
-        return new \GuzzleHttp\Client($configs);
+        return new HttpClient($configs);
     }
 
     /**
@@ -172,13 +187,13 @@ class Client implements QueryInterface
         // Set class name as namespace
         $class = $this->namespace . '\\' . $this->snakeToPascal($name) . 's';
 
-        var_dump("[$class] has been called!");
-
         // Try to create object by name
         $object = new $class($this->config);
 
         // Call user function from endpoint class
         $func = call_user_func_array($object, $arguments);
+
+        dd($func->exec());
 
         return $this->config->get('auto_exec') ? $func->exec() : $func;
     }
@@ -200,8 +215,6 @@ class Client implements QueryInterface
      *
      * @param string $name
      * @param mixed  $value
-     *
-     * @throws \ErrorException
      */
     public function __set(string $name, $value)
     {
@@ -225,23 +238,29 @@ class Client implements QueryInterface
 
         for ($i = 1; $i <= $this->config->get('tries'); $i++) {
 
-            $endpoint = $this->config->get('base_uri') . '/' . $this->config->get('token') . '/' . $url . '.' . $this->config->get('format');
+            $requestEndpoint =
+                $this->config->get('base_uri')
+                . '/'
+                . $this->config->get('token')
+                . '/'
+                . $url
+                . '.'
+                . $this->config->get('format');
 
             if ($this->config->get('verbose')) {
-                error_log("[$type] " . $endpoint);
+                error_log('[' . $type . '] endpoint:' . $requestEndpoint . ' parameters:' . json_encode($params));
             }
 
             if (empty($params)) {
                 // Execute the request to server
-                $result = $this->client->request($type, $endpoint);
+                $result = $this->client->request($type, $requestEndpoint);
             } else {
                 // Execute the request to server
-                $result = $this->client->request($type, $endpoint, [RequestOptions::FORM_PARAMS => $params]);
+                $result = $this->client->request($type, $requestEndpoint, [RequestOptions::FORM_PARAMS => $params]);
             }
 
             // Check the code status
-            $code   = $result->getStatusCode();
-            $reason = $result->getReasonPhrase();
+            $code = $result->getStatusCode();
 
             // If success response from server
             if ($code === 200 || $code === 201) {
@@ -250,7 +269,7 @@ class Client implements QueryInterface
 
             // If not "too many requests", then probably some bug on remote or our side
             if ($code !== 429) {
-                throw new \ErrorException($code . ' ' . $reason);
+                throw new UonTooManyRequests();
             }
 
             // Waiting in seconds
@@ -270,7 +289,7 @@ class Client implements QueryInterface
      */
     public function exec()
     {
-        return $this->doRequest($this->type, $this->endpoint, $this->params, false);
+        return $this->doRequest($this->type, $this->endpoint, $this->params);
     }
 
     /**
@@ -288,22 +307,19 @@ class Client implements QueryInterface
     /**
      * Make the request and analyze the result
      *
-     * @param string $type     Request method
-     * @param string $endpoint Api request endpoint
-     * @param mixed  $params   List of parameters
-     * @param bool   $raw      Return data in raw format
+     * @param string $type            Request method
+     * @param string $requestEndpoint Api request endpoint
+     * @param mixed  $params          List of parameters
+     * @param bool   $raw             Return data in raw format
      *
      * @return null|object|ResponseInterface Array with data, RAW response or NULL if error
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws \ErrorException
      */
-    private function doRequest(string $type, string $endpoint, array $params = [], bool $raw = false)
+    private function doRequest(string $type, string $requestEndpoint, array $params = [], bool $raw = false)
     {
-        // Null by default
-        $response = null;
-
         // Execute the request to server
-        $result = $this->repeatRequest($type, $endpoint, $params);
+        $result = $this->repeatRequest($type, $requestEndpoint, $params);
 
         // If debug then return Guzzle object
         if ($this->config->get('debug') === true) {
@@ -311,11 +327,15 @@ class Client implements QueryInterface
         }
 
         if (null === $result) {
-            throw new \ErrorException('Empty results returned from UOD API');
+            throw new UonEmptyResponseException();
         }
 
         // Return RAW result if required
-        return $raw ? $result : json_decode($result->getBody(), false);
+        if ($raw) {
+            return $result;
+        }
+
+        return json_decode($result->getBody(), false);
     }
 
 }
